4611f7830917708058f9a1445342f13ad8124b66
James Holton <james_holton@yahoo.co.uk>
2019-09-03 15:01:09 +0100

    Added logic to the view to display what us needed

--- lib/app.rb
+++ lib/app.rb
@@ -3,14 +3,23 @@ require 'sinatra'
 set :session_secret, 'secret session'
 
 get '/' do
+  p params
   "Hello !"
 end
 
 get '/secret' do
+  p params
   "top secret message"
 end
 
-get '/cat' do
+get '/random-cat' do
+  p params
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:catname]
   erb(:index)
 end
